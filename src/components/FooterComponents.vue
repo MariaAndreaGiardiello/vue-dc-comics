bf4f19a1be39af6047a09185dfba4a12beaa3571
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-06-05 22:06:53 +0200

    aggiunta prima parte footer

--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -1,5 +1,56 @@
 <template>
-  <footer></footer>
+  <footer>
+      <div class="footer-top">
+          <div class="container">
+              <div class="footer-text">
+              <nav class="column">
+                  <div class="link-title">DC COMICS</div>
+                  <ul>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                  </ul>
+                  <div class="link-title">DC COMICS</div>
+                  <ul>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                  </ul>
+              </nav>
+               <nav class="column">
+                  <div class="link-title">DC COMICS</div>
+                  <ul>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                  </ul>
+              </nav> <nav class="column">
+                  <div class="link-title">DC COMICS</div>
+                  <ul>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                        <li class="link-list-choice"><a href="">Characters</a></li>
+                  </ul>
+              </nav>
+          </div>
+          <div class="footer-logo">
+                <img class="logo-big" src="../assets/img/dc-logo-bg.png" alt="">
+          </div>
+          </div>
+      </div>
+      <div class="footer-bottom"></div>
+  </footer>
 </template>
 
 <script>
@@ -9,5 +60,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+a {
+    text-decoration: none;
+}
+.footer-top {
+    width: 100%;
+    height: 250px;
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-direction: row;
+}
+.footer-text {
+    width: calc(100% / 2);
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+  li {
+    list-style: none;
+    font-size: 12px;
+  }
+.column {
+    width: calc(100% / 3);
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    text-align: left;
+}
+.footer-logo {
+    width: calc(100% / 2);
+}
 
+.logo-big {
+    width: 300px;
+    height: 300px;
+}
 </style>
